d5f0db7b388e131efd3a61a15ba5a74ee4510dd5
sytem <sytem.out@hotmail.com>
2019-06-20 00:29:56 +0800

    数据结构之单链表

--- src/list/SingleLinkedList.java
+++ src/list/SingleLinkedList.java
@@ -2,6 +2,7 @@ package list;
 
 /**
  * 数据结构之单链表
+ *
  * @author 严书航
  */
 public class SingleLinkedList<E> {
@@ -30,7 +31,208 @@ public class SingleLinkedList<E> {
     }
 
     /**
+     * 添加元素
+     *
+     * @param element
+     * @return
+     */
+    public boolean add(E element) {
+        // 新节点
+        Node<E> newNode = new Node<>(element);
+        if (size == 0) {
+            first = newNode;
+            size++;
+            return true;
+        }
+        // 获得最后一个节点
+        Node<E> lastNode = getNode(size - 1);
+        // 新节点添加到最后一个节点
+        lastNode.addNext(newNode);
+        // 元素数量+1
+        size++;
+        return true;
+    }
+
+    public E get(int index) {
+        indexCheck(index);
+        return getNode(index).element;
+    }
+
+    public E getFirst() {
+        return first.element;
+    }
+
+    public E removeFirst() {
+        if (first == null) {
+            return null;
+        }
+        E oldVal = first.element;
+        first = first.next;
+        size--;
+        return oldVal;
+    }
+
+    public E getLast() {
+        return getNode(size - 1).element;
+    }
+
+    public E set(int index, E element) {
+        indexCheck(index);
+        Node<E> node = getNode(index);
+        E oldVal = node.element;
+        node.element = element;
+        return oldVal;
+    }
+
+    public boolean insert(int index, E element) {
+        indexCheck(index);
+        Node<E> node = new Node<>(element);
+        if (index == 0) {
+            node.next = first;
+            first = node;
+            size++;
+            return true;
+        }
+        Node<E> preNode = getNode(index - 1);
+        node.next = preNode.next;
+        preNode.next = node;
+        size++;
+        return true;
+    }
+
+    /**
+     * 返回第一个匹配到的节点索引
+     *
+     * @param element 要查找的元素
+     * @return 查找到返回index值，为查找到返回-1
+     */
+    public int indexOf(E element) {
+        int index = 0;
+        if (element == null) {
+            for (Node<E> node = first; node != null; node = node.next) {
+                if (null == node.element) {
+                    return index;
+                }
+                index++;
+            }
+        } else {
+            for (Node<E> node = first; node != null; node = node.next) {
+                if (element.equals(node.element)) {
+                    return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 返回最后一个匹配到的节点索引
+     *
+     * @param element 匹配的元素
+     * @return 未匹配到返回 -1
+     */
+    public int lastIndexOf(E element) {
+        int index = 0;
+        int lastIndex = -1;
+        if (element != null) {
+            for (Node<E> node = first; node != null; node = node.next) {
+                if (element.equals(node.element)) {
+                    lastIndex = index;
+                }
+                index++;
+            }
+            return lastIndex;
+        } else {
+            for (Node<E> node = first; node != null; node = node.next) {
+                if (node.element == null) {
+                    lastIndex = index;
+                }
+                index++;
+            }
+            return lastIndex;
+        }
+    }
+
+    /**
+     * 删除索引位置的元素
+     *
+     * @param index 索引
+     * @return 返回被删除的元素
+     */
+    public E remove(int index) {
+        indexCheck(index);
+        if (index == 0) {
+            return removeFirst();
+        }
+        Node<E> preNode = getNode(index - 1);
+        Node<E> currNode = preNode.next;
+        preNode.next = currNode.next;
+        size--;
+        return currNode.element;
+    }
+
+    /**
+     * @param element 删除的元素
+     * @return 没有匹配的元素返回false，删除成功返回true
+     */
+    public boolean remove(E element) {
+        int index = indexOf(element);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+        int index = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (Node<E> node = first; node != null; node = node.next) {
+            stringBuilder.append(node.element);
+            index++;
+            if (index != size) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 遍历列表获得当前索引对应的元素
+     *
+     * @param index 索引
+     * @return 索引对应的元素
+     */
+    private Node<E> getNode(int index) {
+        indexCheck(index);
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    /**
+     * 检查索引的正确性
+     *
+     * @param index 索引
+     */
+    private void indexCheck(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("SingleLinkedList size " + size + " index " + index);
+        }
+    }
+
+    /**
      * 链表中存储的节点
+     *
      * @param <E>
      */
     private static class Node<E> {
